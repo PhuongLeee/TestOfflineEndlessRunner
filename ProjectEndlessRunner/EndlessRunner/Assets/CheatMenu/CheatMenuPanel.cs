724fc744a25e18d504269b15637783f71ca1655c
F:\MySetup\MyApplication\GitHub\SetUp <lepg.hn@gmail.com>
2021-06-30 02:54:28 +0700

    2. add cheat menu

---
+++ ProjectEndlessRunner/EndlessRunner/Assets/CheatMenu/CheatMenuPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheatMenuPanel : MonoBehaviour
+{
+    private PlayerData playerdata;
+    private void Start() {
+        playerdata = PlayerData.instance;
+    }
+    public void OnButtonAddCoins() {
+        playerdata.coins += 100;
+        playerdata.Save();
+    } 
+
+    public void OnButtonAddPremium() {
+        playerdata.premium += 100;
+        playerdata.Save();
+    }
+
+    public void OnButtonAddComsumables() {
+        for (int i = 0; i < ShopItemList.s_ConsumablesTypes.Length; ++i) {
+            var c = ConsumableDatabase.GetConsumbale(ShopItemList.s_ConsumablesTypes[i]);
+            if (c != null) {
+                playerdata.consumables[c.GetConsumableType()] += 10;
+            }
+        }
+
+        playerdata.Save();
+    }
+
+    public void OnButtonClose() {
+        gameObject.SetActive(false);
+    }
+}
---
+++ ProjectEndlessRunner/EndlessRunner/Assets/CheatMenu/CheckOpenCheatMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckOpenCheatMenu : MonoBehaviour
+{
+    private int checkL = 0;
+    [SerializeField] RectTransform cheatPanel;
+    public void CheckOpenCheatLeft() {
+        if (checkL % 2==0) {            
+            checkL++;
+            if (checkL == 5) {
+                cheatPanel.gameObject.SetActive(true);
+                checkL = 0;
+            }
+        } else checkL = 0;
+    }
+    public void CheckOpenCheatRight() {
+        if (checkL % 2 != 0) checkL++;
+        else checkL = 0;
+    }
+
+    private void OnEnable() {
+        checkL = 0;
+    }
+}
